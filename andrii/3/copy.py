956709012cd308aed9319e790af7fdf1a826f687
Yan <yanc@ext.inditex.com>
2024-08-09 13:04:47 +0200

    1

--- andrii/3/copy.py
+++ andrii/3/copy.py
@@ -1,30 +1,7 @@
 import shutil
 import sys
-import logging
 from pathlib import Path
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-
-def copy_tree(src, dst):
-  src_path = Path(src)
-  dst_path = Path(dst)
-  
-  shutil.copytree(src_path, dst_path)
-      
-if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    logger.error('Usage: python script.py <source_directory> <destination_directory>')
-    sys.exit(1)
-
-  source_dir = sys.argv[1]
-  destination_dir = sys.argv[2]
-
-  try:
-    copy_tree(source_dir, destination_dir)
-    logger.info(f"Files copied from '{source_dir}' to '{destination_dir}' successfully.")
-  except Exception as e:
-    logger.error('An error occurred during file copying.')
-    logger.exception(e)
-    sys.exit(1)
+src_path = Path(sys.argv[1])
+dst_path = Path(sys.argv[2])  
+shutil.copytree(src_path, dst_path)
